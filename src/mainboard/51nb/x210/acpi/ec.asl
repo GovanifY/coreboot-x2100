b1f61af72a960918056fd9e4bbfc23f52ef86290
Gauvain Roussel-Tarbouriech <gauvain@govanify.com>
2022-01-09 02:22:53 +0100

    Add support for 51NB X2100
    This is an updated variant of the X210, which shares enough hardware for
    it to be implemented as a variant rather than a new board
    
    Co-authored-by: Matthew Garrett <mjg59@srcf.ucam.org>

--- src/mainboard/51nb/x210/acpi/ec.asl
+++ src/mainboard/51nb/x210/acpi/ec.asl
@@ -5,7 +5,11 @@ Device (EC)
 	Name (_HID, EisaId ("PNP0C09"))
 	Name (_UID, 0)
 
-	Name (_GPE, 0x4F)  // _GPE: General Purpose Events
+#if CONFIG_BOARD_51NB_X210
+    Name (_GPE, 0x4F)  // _GPE: General Purpose Events
+#else
+    Name (_GPE, 0x50)  // _GPE: General Purpose Events
+#endif
 	Name (_CRS, ResourceTemplate () {
 		IO (Decode16, 0x62, 0x62, 1, 1)
 		IO (Decode16, 0x66, 0x66, 1, 1)
--- src/mainboard/51nb/x210/mainboard.c
+++ src/mainboard/51nb/x210/mainboard.c
@@ -1,14 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0-or-later */
 
 #include <soc/ramstage.h>
-#include "gpio.h"
-
-void mainboard_silicon_init_params(FSP_SIL_UPD *params)
-{
-	/* Configure pads prior to SiliconInit() in case there's any
-	 * dependencies during hardware initialization. */
-	gpio_configure_pads(gpio_table, ARRAY_SIZE(gpio_table));
-}
 
 static void mainboard_enable(struct device *dev)
 {
